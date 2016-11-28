4e467d1036171c7bbf748d7a2a25185d259ceeb8
Hydorah <az950512@sjtu.edu.cn>
2016-11-28 23:54:46 +0800

    4

--- script/wikidataDB.py
+++ script/wikidataDB.py
@@ -2,10 +2,19 @@ import json
 import pymysql
 import time
 
+def truncate all():
+    cursor.execute("TRUNCATE TABLE datavalue_globecoordinate")
+    cursor.execute("TRUNCATE TABLE datavalue_quantity")
+    cursor.execute("TRUNCATE TABLE datavalue_string")
+    cursor.execute("TRUNCATE TABLE datavalue_time")
+    cursor.execute("TRUNCATE TABLE datavalue_wikibase")
+    cursor.execute("TRUNCATE TABLE description")
+    cursor.execute("TRUNCATE TABLE entity")
+    cursor.execute("TRUNCATE TABLE mainsnak")
+    cursor.execute("TRUNCATE TABLE qualifier")
+
 def insert():
-
     testFile = open("smallwikidata-latest-all3.json")
-
     for data in json.load(testFile):
 
         # print(type(data))
@@ -13,9 +22,16 @@ def insert():
         entity_id = data["id"]
         entity_lang = "en"
         entity_type = data["type"]
-        entity_desc = data["descriptions"]["en"]["value"]
-        desc_lang = data["descriptions"]["en"]["language"]
-        entity_text = data["labels"]["en"]["value"]
+        if "en" in data["descriptions"]:
+            entity_desc = data["descriptions"]["en"]["value"]
+            desc_lang = data["descriptions"]["en"]["language"]
+        else:
+            entity_desc = "N/A"
+            desc_lang = "N/A"
+        if "en" in data["labels"]:
+            entity_text = data["labels"]["en"]["value"]
+        else:
+            entity_text = "N/A"
         claims = data["claims"]
 
         # print("entity_id: " + entity_id)
@@ -33,11 +49,9 @@ def insert():
                 claim_type = property[serial]["type"]
                 rank = property[serial]["rank"]
                 mainsnak = property[serial]["mainsnak"]
-                datavalue = mainsnak["datavalue"]
-                datavalue_type = datavalue["type"]
-                datavalue_value = datavalue["value"]
                 snak_id = property[serial]["id"]
                 snak_type = mainsnak["snaktype"]
+                data_type = mainsnak["datatype"]
 
                 if "qualifiers" in property[serial]:
                     qualifiers = property[serial]["qualifiers"]
@@ -50,35 +64,38 @@ def insert():
                             q_data_type = qualifiers[pid][i]["datatype"]
                             cursor.execute("INSERT INTO qualifier(hash, snaktype, property_id, datatype) VALUES (%s, %s, %s, %s)", (q_hash, q_snak_type, q_property_id, q_data_type))
 
-                cursor.execute("INSERT INTO mainsnak VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (snak_id, entity_id, property_id, serial, claim_type, snak_type, datavalue_type, rank))
-
-                if datavalue_type == "string":
-                    value = datavalue_value
-                    cursor.execute("INSERT INTO datavalue_string VALUES (%s, %s)", (snak_id, value))
-                elif datavalue_type == "wikibase-entityid":
-                    wikibase_entityid = datavalue_value["id"]
-                    cursor.execute("INSERT INTO datavalue_wikibase VALUES (%s, %s)", (snak_id, wikibase_entityid))
-                elif datavalue_type == "time":
-                    time = datavalue_value["time"]
-                    timezone = datavalue_value["timezone"]
-                    before = datavalue_value["before"]
-                    after = datavalue_value["after"]
-                    precision = datavalue_value["precision"]
-                    calendarmodel = datavalue_value["calendarmodel"]
-                    cursor.execute("INSERT INTO datavalue_time VALUES (%s, %s, %s, %s, %s, %s, %s)", (snak_id, time, timezone, before, after, precision, calendarmodel))
-                elif datavalue_type == "globecoordinate":
-                    latitude = datavalue_value["latitude"]
-                    longitude = datavalue_value["longitude"]
-                    altitude = datavalue_value["altitude"]
-                    precision = datavalue_value["precision"]
-                    globe = datavalue_value["globe"]
-                    cursor.execute("INSERT INTO datavalue_globecoordinate VALUES (%s, %s, %s, %s, %s, %s)", (snak_id, latitude, longitude, altitude, precision, globe))
-                elif datavalue_type == "quantity":
-                    amount = datavalue_value["amount"]
-                    upperBound = datavalue_value["upperBound"]
-                    lowerBound = datavalue_value["lowerBound"]
-                    #unit = datavalue_value["unit"]
-                    cursor.execute("INSERT INTO datavalue_quantity VALUES (%s, %s, %s, %s, 'default')", (snak_id, amount, upperBound, lowerBound))
+                cursor.execute("INSERT INTO mainsnak VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (snak_id, entity_id, property_id, serial, claim_type, snak_type, data_type, rank))
+                if "datavalue" in mainsnak:
+                    datavalue = mainsnak["datavalue"]
+                    datavalue_type = datavalue["type"]
+                    datavalue_value = datavalue["value"]
+                    if datavalue_type == "string":
+                        value = datavalue_value
+                        cursor.execute("INSERT INTO datavalue_string VALUES (%s, %s)", (snak_id, value))
+                    elif datavalue_type == "wikibase-entityid":
+                        wikibase_entityid = datavalue_value["id"]
+                        cursor.execute("INSERT INTO datavalue_wikibase VALUES (%s, %s)", (snak_id, wikibase_entityid))
+                    elif datavalue_type == "time":
+                        time = datavalue_value["time"]
+                        timezone = datavalue_value["timezone"]
+                        before = datavalue_value["before"]
+                        after = datavalue_value["after"]
+                        precision = datavalue_value["precision"]
+                        calendarmodel = datavalue_value["calendarmodel"]
+                        cursor.execute("INSERT INTO datavalue_time VALUES (%s, %s, %s, %s, %s, %s, %s)", (snak_id, time, timezone, before, after, precision, calendarmodel))
+                    elif datavalue_type == "globecoordinate":
+                        latitude = datavalue_value["latitude"]
+                        longitude = datavalue_value["longitude"]
+                        altitude = datavalue_value["altitude"]
+                        precision = datavalue_value["precision"]
+                        globe = datavalue_value["globe"]
+                        cursor.execute("INSERT INTO datavalue_globecoordinate VALUES (%s, %s, %s, %s, %s, %s)", (snak_id, latitude, longitude, altitude, precision, globe))
+                    elif datavalue_type == "quantity":
+                        amount = datavalue_value["amount"]
+                        upperBound = datavalue_value["upperBound"]
+                        lowerBound = datavalue_value["lowerBound"]
+                        #unit = datavalue_value["unit"]
+                        cursor.execute("INSERT INTO datavalue_quantity VALUES (%s, %s, %s, %s, 'default')", (snak_id, amount, upperBound, lowerBound))
 
 
                 # print("property_id: " + property_id)
@@ -90,7 +107,6 @@ def insert():
                 # print("\n")    
 
 if __name__ == '__main__':
-    # print('start')
     connection = pymysql.connect(host='127.0.0.1',
                                  user='root',
                                  password='',
@@ -99,7 +115,6 @@ if __name__ == '__main__':
                                  port=3306,
                                  cursorclass=pymysql.cursors.DictCursor)
     cursor = connection.cursor()
-    # print('connected')
     insert()
     connection.commit()
 
